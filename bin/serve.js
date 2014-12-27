7660b9ee8988337c3e3a2aba14ceddabf442e44f
AJ ONeal <awesome@coolaj86.com>
2014-12-27 13:35:24 -0700

    more helpful output

--- bin/serve.js
+++ bin/serve.js
@@ -1,14 +1,28 @@
 'use strict';
 
 var http = require('http')
-  , serve = require('../server')
-  , port
-  , server
+  , port = process.argv[2] || 3000
+  , server = http.createServer(require('../server').create())
   ;
 
-port = port || process.argv[2] || 3000;
-
-server = http.createServer(serve.create());
 server.listen(port, function() {
-  console.log('Listening on port ' + port);
+  console.log('Listening on http://localhost:' + port);
+  
+  console.log('Test these links in your browser:');
+  console.log('    http://local.foobar3000.com:' + port
+    + '/resize/api'
+    + '?state=1'
+    + '&url=http://i.imgur.com/57RKZdJ.jpg'
+  );
+  console.log('    http://local.foobar3000.com:' + port
+    + '/resize/api'
+    + '?state=1'
+    + '&width=200'
+    + '&height=300'
+    + '&format=gif'
+    + '&quality=85'
+    + '&url=http://i.imgur.com/57RKZdJ.jpg'
+  );
+
+  console.log('');
 });
