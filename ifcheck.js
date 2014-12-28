760c00ac367b9588df7817a7219373af295bb2cf
root <root@delete.me>
2014-12-27 21:51:10 -0500

    auto-check ip address

---
+++ ifcheck.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var PromiseA = require('bluebird')
+  , request = PromiseA.promisify(require('request'))
+  ;
+
+function checkip(ip) {
+  var os = require('os');
+  var ifaces = os.networkInterfaces();
+
+  return Object.keys(ifaces).some(function (ifname) {
+    return ifaces[ifname].some(function (iface) {
+      return iface.address === ip;
+    });
+  });
+}
+
+function getExternalIp() {
+  return request({ url: 'http://checkip.hellabit.com' }).spread(function (req, data) {
+    return checkip(data) && data;
+  });
+}
+
+module.exports.getExternalIp = getExternalIp;
+
+// en0 192.168.1.101
+// eth0 10.0.0.101
