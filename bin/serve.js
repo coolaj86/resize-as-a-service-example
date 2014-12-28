f3dd986149dddc267482cfd538b41ec7a76831ef
AJ ONeal <awesome@coolaj86.com>
2014-12-27 21:57:27 -0500

    add auto-ip config

--- bin/serve.js
+++ bin/serve.js
@@ -2,28 +2,29 @@
 
 var http = require('http')
   , port = process.argv[2] || 3000
+  , ifcheck = require('../ifcheck')
   , server = http.createServer(require('../server').create())
   ;
 
-server.listen(port, function() {
-  console.log('Listening on http://localhost:' + port);
-  console.log('');
-  
-  console.log('Test these links in your browser:');
-  console.log('    http://local.foobar3000.com:' + port
-    + '/resize/api'
-    + '?state=1'
-    + '&url=http://i.imgur.com/qqpxDmJ.jpg'
-  );
-  console.log('    http://local.foobar3000.com:' + port
-    + '/resize/api'
-    + '?state=1'
-    + '&width=200'
-    + '&height=300'
-    + '&format=gif'
-    + '&quality=85'
-    + '&url=http://i.imgur.com/qqpxDmJ.jpg'
-  );
+ifcheck.getExternalIp().then(function (ip) {
+  var host = ip || 'localhost'
+    , fqdn = ip || 'local.foobar3000.com'
+    ;
 
-  console.log('');
+  server.listen(port, function() {
+    console.log('\n', 'Listening on http://' + host + ':' + port, '\n');
+    
+    console.log('Test these links in your browser:');
+    console.log('    http://' + fqdn + ':' + port
+      + '/resize/api?state=1'
+      + '&url=http://i.imgur.com/qqpxDmJ.jpg'
+    );
+    console.log('    http://' + fqdn + ':' + port
+      + '/resize/api?'
+      + ['state=1', 'width=200', 'height=300', 'format=gif', 'quality=85'].join('&')
+      + '&url=http://i.imgur.com/qqpxDmJ.jpg'
+    );
+
+    console.log('');
+  });
 });
